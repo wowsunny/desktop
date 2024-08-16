8bf784c463e2bb321461e1eda4e95319199803f6
Kendal Cormany <kendal.cormany@nexus.io>
2024-08-16 14:40:38 -0700

    You need ASAR

--- forge.config.ts
+++ forge.config.ts
@@ -10,7 +10,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
 const config: ForgeConfig = {
   packagerConfig: {
-    asar: false,
+    asar: true,
     osxSign: {
       optionsForFile: (filepath) => {
         return { entitlements: './assets/entitlements.mac.plist' };
