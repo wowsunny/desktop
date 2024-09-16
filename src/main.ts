5ec327f31b815b264fb83350839412ab6c2968c3
Robin Huang <robin.j.huang@gmail.com>
2024-09-16 14:01:58 +0900

    Hide top level menu bar.

--- src/main.ts
+++ src/main.ts
@@ -46,6 +46,7 @@ const createWindow = async () => {
       nodeIntegration: true, // Enable Node.js integration
       contextIsolation: true,
     },
+    autoHideMenuBar: true,
   });
 
   // and load the index.html of the app.
