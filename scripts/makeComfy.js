112361b47ba9fa6a535a0ab35ad374d24a3948f0
Robin Huang <robin.j.huang@gmail.com>
2024-11-11 21:18:51 -0800

    Rename ComfyUI-Manager to manager-core. (#227)
    * Rename ComfyUI-Manager to manager-core.
    
    * Checkout specific manager core commit.

--- scripts/makeComfy.js
+++ scripts/makeComfy.js
@@ -1,5 +1,6 @@
 const { execSync } = require('child_process');
 const pkg = require('../package.json');
+const fs = require('fs');
 
 function makeAssets(gpuFlag) {
   const baseCommand = [
@@ -8,18 +9,25 @@ function makeAssets(gpuFlag) {
     `comfy-cli --skip-prompt --here install --version ${pkg.config.comfyVersion} --fast-deps`,
     gpuFlag,
     '--manager-url https://github.com/Comfy-Org/manager-core',
+    '--manager-commit',
+    pkg.config.managerCommit,
     '&&',
     'comfy-cli --here standalone',
     '&&',
     'yarn run make:frontend'
   ].join(' ');
 
-  // Special case for macOS which needs additional checks
   if (gpuFlag === '--m-series') {
-    return execSync(`${baseCommand} && ../scripts/checkAssetsMacos.sh python`, { stdio: 'inherit' });
+    execSync(`${baseCommand} && ../scripts/checkAssetsMacos.sh python`, { stdio: 'inherit' });
+  } else {
+    execSync(baseCommand, { stdio: 'inherit' });
   }
-
-  execSync(baseCommand, { stdio: 'inherit' });
+  
+  // Rename custom_nodes/ComfyUI-Manager to manager-core
+  if (!fs.existsSync('assets/ComfyUI/custom_nodes/ComfyUI-Manager')) {
+    throw new Error('ComfyUI-Manager not found');
+  }
+  fs.renameSync('./assets/ComfyUI/custom_nodes/ComfyUI-Manager', './assets/ComfyUI/custom_nodes/manager-core');
 }
 
 // Get GPU flag from command line argument
