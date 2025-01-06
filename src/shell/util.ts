1e15f5c38f60935faf0611298280e1db17e4cdec
filtered <176114999+webfiltered@users.noreply.github.com>
2025-01-07 03:06:31 +1100

    Update package: systeminformation - uses full PowerShell path (#590)
    * Update package: systeminformation - PowerShell PATH
    
    https://github.com/sebhildebrandt/systeminformation/issues/957
    
    * Fix cannot start ComfyUI: File not found
    
    - Resolves various issues caused at different installation states, when PowerShell is not in PATH
    - Forces standard env for all users - uses Windows PowerShell even if PS core is default

--- src/shell/util.ts
+++ src/shell/util.ts
@@ -3,7 +3,8 @@ import os from 'node:os';
 export function getDefaultShell(): string {
   switch (os.platform()) {
     case 'win32':
-      return 'powershell.exe';
+      // Use full path to avoid e.g. https://github.com/Comfy-Org/desktop/issues/584
+      return `${process.env.SYSTEMROOT}\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`;
     case 'darwin':
       return 'zsh';
     default: // Linux and others
