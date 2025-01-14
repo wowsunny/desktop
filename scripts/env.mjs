ff71b183f075ec5c1c18e53ed4672eb0a678cd5d
Chenlei Hu <hcl@comfy.org>
2025-01-14 14:52:55 -0500

    [Cleanup] Remove unused file env.mjs (#621)

--- scripts/env.mjs
+++
@@ -1,7 +0,0 @@
-import fs from 'node:fs/promises';
-
-const envContent = `# env vars picked up by the ComfyUI executable on startup
-COMFYUI_CPU_ONLY=true
-`;
-
-await fs.writeFile('ComfyUI/.env', envContent);
